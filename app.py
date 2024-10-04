fe4988af711230d3d3fb41713f1c063b5f09eb1e
Ronaldo Gandra Rocha <ronaldogandrarocha@gmail.com>
2024-10-04 17:51:23 -0300

    testando

--- app.py
+++ app.py
@@ -5,6 +5,14 @@ from datetime import datetime
 
 app = Flask(__name__)
 
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
 @app.route('/')
 def index():
   return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True, use_reloader=True)
